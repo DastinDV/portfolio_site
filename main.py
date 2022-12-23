74187db8b04c0ad767e75c3cf2d20af0973e069b
DastinDV <komander_72@rambler.ru>
2022-12-23 15:43:55 +0300

    added descriptions and titles and images

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Ivan Zverkov")
+    content = "I am just a programmer. Working for IBA company and try to enter gamedev!"
+    st.info(content)
+
+st.write("Hello my first very diffucult student task. Yo Yo Yo Mister White")
+
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
